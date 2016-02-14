3cde703dd9e311c210e8716f960911ae51934e34
Hans <hans.php@gmail.com>
2016-02-14 14:15:04 -0600

    Archivo de configuración

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+define('__HVE', "<tt>Error crítico de seguridad</tt>");
+include_once('class/MySQL/mysqli.dev.class.php');
+
+define('H_MYSQL_HOST', 'localhost');
+define('H_MYSQL_USUARIO', 'root');
+define('H_MYSQL_CLAVE', '');
+define('H_MYSQL_BD', '');
+
+function clear($s){
+	return trim(addslashes($s));
+}
+
+if(!empty($_POST)){
+	foreach($_POST as &$r){
+		$r = clear($r);
+	}
+}
+
+$SQL = new H_MYSQLi;
