c21eba88bd8d5f0ace5628a637f08a5f84f073b8
Hans <hans.php@gmail.com>
2016-02-15 09:45:14 -0600

    Consulta de columnas

--- load.php
+++ load.php
@@ -1,8 +1,15 @@
 <?php
 header('Content-Type: application/json');
 include_once("config.php");
+$result = array('error' => 1);
 
-$tables = $SQL->consulta("SELECT TABLE_NAME, ENGINE, TABLE_ROWS, TABLE_COLLATION, TABLE_COMMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = '{$_GET['database']}' AND TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA <> 'information_schema'");
+if(isset($_GET['type'])){
+	if($_GET['type'] == 'table'){
+		$result = $SQL->consulta("SELECT TABLE_NAME, ENGINE, TABLE_ROWS, TABLE_COLLATION, TABLE_COMMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = '{$_GET['database']}' AND TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA <> 'information_schema'");
+	}elseif($_GET['type'] == 'column'){
+		$result = $SQL->consulta("SELECT TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, DATA_TYPE, IFNULL(CHARACTER_MAXIMUM_LENGTH, IFNULL(NUMERIC_PRECISION, DATETIME_PRECISION)) MAX_LEN, COLLATION_NAME, COLUMN_TYPE FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '{$_GET['database']}' AND TABLE_SCHEMA <> 'information_schema' AND TABLE_NAME = '{$_GET['table']}' ORDER BY ORDINAL_POSITION");
+	}
+}
 
-echo json_encode($tables);
+echo json_encode($result);
 
